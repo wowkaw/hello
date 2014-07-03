c008c5f3c93d48f2bb5446ce31ae65bb5f13a88c
Wei-Chih Chen <Wei-Chih_Chen@chintekiMacBook-Air.local>
2014-07-03 15:38:45 +0800

    Creat Hello World

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  get "hello" => "hello#index"
+  root to: "hello#index"
 end
